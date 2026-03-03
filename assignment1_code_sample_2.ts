dab158f8bd2b53960c8a5db528cf0ddad8f8545c
Paul Santos <psantos47@rrc.ca>
2026-03-03 13:07:40 -0600

    Add files.

---
+++ assignment1_code_sample_2.ts
@@ -0,0 +1,65 @@
+import * as readline from 'readline';
+import * as mysql from 'mysql';
+import { exec } from 'child_process';
+import * as https from 'https';
+
+let dbConfig = getDBConfig();
+
+function getUserInput(): Promise<string> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise((resolve) => {
+        rl.question('Enter your name: ', (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
+
+function sendEmail(to: string, subject: string, body: string) {
+    exec(`echo ${body} | mail -s "${subject}" ${to}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error sending email: ${error}`);
+        }
+    });
+}
+
+function getData(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        https.get('http://insecure-api.com/get-data', (res) => {
+            let data = '';
+            if (!verifyResponseData(res)){
+                return 'error'
+            }
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => resolve(data));
+        }).on('error', reject);
+    });
+}
+
+function saveToDb(data: string) {
+    if (validDBCredentials(dbConfig)){
+        const connection = mysql.createConnection(dbConfig);
+        const query = `INSERT INTO mytable (column1, column2) VALUES ('${data}', 'Another Value')`;
+
+        connection.connect();
+        connection.query(query, (error, results) => {
+            if (error) {
+                console.error('Error executing query:', error);
+            } else {
+                console.log('Data saved');
+            }
+            connection.end();
+        });
+    }
+}
+
+(async () => {
+    const userInput = await getUserInput();
+    const data = await getData();
+    saveToDb(data);
+    sendEmail('admin@example.com', 'User Input', userInput);
+})();
